28c2b860e18a45145852eeae5194ab6f20d2a888
marcinpirog <158519572+marcinp800@users.noreply.github.com>
2024-11-27 19:08:26 +0100

    mm

---
+++ Arithmetic.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+x=$1
+y=$2
+
+echo "Uzycie polecenia let"
+
+let res=x+y
+echo $res
+
+echo
+
+echo "Uzycie (())"
+
+res=$((x+y))
+echo $res
+
+echo
+echo "Uzycie []"
+res=$[x+y]
+echo $res
